1e10e8a1d5880e13947b7d3c2c3a83ba9e99533b
burakguldiken1@gmail.com <burakguldiken1@gmail.com>
2021-03-24 14:56:30 +0300

    backend created

---
+++ backend/Purple/Extensions/ServiceExtensions.cs
@@ -0,0 +1,135 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Purple.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static IConfiguration Configuration;
+
+        #region Dependency Injection
+        public static void ConfigureDependencies(this IServiceCollection services)
+        {
+            //AddScoped
+        }
+        #endregion
+
+        #region SwaggerConfiguration
+        public static void SwaggerConfiguration(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("Purple", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Purple Project",
+                    Description = "Purple Project Version 1 Web Api",
+                });
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Please insert JWT with Bearer into field",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+                       {
+                         new OpenApiSecurityScheme
+                         {
+                           Reference = new OpenApiReference
+                           {
+                             Type = ReferenceType.SecurityScheme,
+                             Id = "Bearer"
+                           }
+                          },
+                          new string[] { }
+                        }
+                      });
+
+                var xmlFile = $"{System.Reflection.Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                c.IncludeXmlComments(System.IO.Path.Combine(AppContext.BaseDirectory, xmlFile));
+            });
+        }
+        #endregion
+
+        #region Header Configuration
+        public static void HeaderConfiguration(this IServiceCollection services)
+        {
+            services.Configure<ForwardedHeadersOptions>(options =>
+            {
+                options.ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
+            });
+        }
+        #endregion
+
+        #region AddMiddleware
+        public static void ConfigureCors(this IServiceCollection services)
+        {
+            services.AddCors();
+            services.AddControllers()
+                .AddNewtonsoftJson(options =>
+                {
+                    options.SerializerSettings.ContractResolver = new DefaultContractResolver();
+                });
+            services.AddMvc(options =>
+            {
+                options.EnableEndpointRouting = false;
+            });
+
+            services.AddHttpContextAccessor();
+            services.AddMemoryCache();
+        }
+        #endregion
+
+        #region Jwt
+        public static void JwtConfigure(this IServiceCollection services)
+        {
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }
+             )
+             .AddJwtBearer(options =>
+             {
+                 options.TokenValidationParameters = new TokenValidationParameters
+                 {
+                     ValidateActor = true,
+                     ValidateAudience = false,
+                     ValidateIssuer = false,
+                     ValidateLifetime = true,
+                     ValidateIssuerSigningKey = true,
+                     ClockSkew = TimeSpan.FromHours(1),
+                     IssuerSigningKey = new SymmetricSecurityKey(
+                         Encoding.UTF8.GetBytes("PurpleBackendApiVersion1"))
+
+                 };
+
+                 options.Events = new JwtBearerEvents
+                 {
+                     OnTokenValidated = context =>
+                     {
+                         return Task.CompletedTask;
+                     },
+                     OnAuthenticationFailed = context =>
+                     {
+                         Console.WriteLine("Exception:{0}", context.Exception.Message);
+                         return Task.CompletedTask;
+                     }
+                 };
+             });
+        }
+        #endregion
+    }
+}
